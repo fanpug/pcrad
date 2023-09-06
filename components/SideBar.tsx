fe6d1c8407d6257ae4195c31c123805dbfa0cce1
Humberto Navarro <a320669@uach.mx>
2023-09-06 09:27:33 -0600

    Initial setup

---
+++ components/SideBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  HiOutlineClipboardList,
+  HiOutlineDocumentReport,
+} from "react-icons/hi";
+import { BsPersonGear } from "react-icons/bs";
+
+const SideBar = () => {
+  return (
+    <nav
+      className="fixed top-0 left-0 h-screen w-16 flex flex-col
+                  bg-gray-900 shadow-lg"
+    >
+      <SideBarIcon icon={<HiOutlineClipboardList size="28" />} text="Captura" />
+      <SideBarIcon icon={<HiOutlineDocumentReport size="32" />} text="Reporte" />
+      <SideBarIcon icon={<BsPersonGear size="20" />} text="Docentes" />
+    </nav>
+  );
+};
+
+const SideBarIcon = ({ icon, text = "tooltip 💡"  }) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+  </div>
+);
+
+export default SideBar;
