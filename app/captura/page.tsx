ee58ef2d4637069d4d90998e815624fec9cb966c
Humberto Navarro <a320669@uach.mx>
2023-09-07 12:04:34 -0600

    Add handleonclick function to squarebutton component

--- app/captura/page.tsx
+++ app/captura/page.tsx
@@ -1,24 +1,52 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { SetStateAction, useState } from "react";
 
 import SquareButton from "@/components/SquareButton";
 
 const Page = () => {
   const [floor, setFloor] = useState(0);
+  const [classroom, setClassroom] = useState("0");
+
+  const handleSetFloor = (floorText: SetStateAction<String>) => {
+    let floorNum = floorText === "PB" ? 0 : Number(floorText);
+    setFloor(floorNum);
+    console.log(floor);
+    
+  };
 
   return (
     <main className="p-5 pl-24 pr-8 w-full">
       <div id="button-group">
         <div id="floor-buttons" className="flex gap-10">
-          <SquareButton innerText="PB" lowerText="Planta Baja" />
-          <SquareButton innerText="1" lowerText="Primer piso" />
-          <SquareButton innerText="2" lowerText="Segundo piso" />
-          <SquareButton innerText="3" lowerText="Tercer piso" />
+          <SquareButton
+            innerText="PB"
+            lowerText="Planta Baja"
+            handleOnClick={handleSetFloor}
+          />
+          <SquareButton
+            innerText="1"
+            lowerText="Primer piso"
+            handleOnClick={handleSetFloor}
+          />
+          <SquareButton
+            innerText="2"
+            lowerText="Segundo piso"
+            handleOnClick={handleSetFloor}
+          />
+          <SquareButton
+            innerText="3"
+            lowerText="Tercer piso"
+            handleOnClick={handleSetFloor}
+          />
+        </div>
+        <div id="classroom-buttons">
+
         </div>
       </div>
     </main>
   );
 };
+
 
 export default Page;
--- components/SquareButton.tsx
+++ components/SquareButton.tsx
@@ -1,8 +1,17 @@
-const SquareButton = ({ innerText, lowerText }) => {
+const SquareButton = ({ innerText, lowerText, handleOnClick }) => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <button className="flex items-center justify-center h-16 w-16 my-2 mx-auto bg-white hover:bg-[#f8abf8] focus:bg-[#E28EE2] border-[1.5px] border-black text-[#371E49] text-3xl font-bold rounded-xl transition-all duration-100 ease-linear shadow-md">{innerText}</button>
-      <span className="text-center text-xs leading-[14px] w-10 -m-1">{lowerText}</span>
+      <button
+        className="flex items-center justify-center h-16 w-16 my-2 mx-auto bg-white hover:bg-[#f8abf8] focus:bg-[#E28EE2] border-[1.5px] border-black text-[#371E49] text-3xl font-bold rounded-xl transition-all duration-100 ease-linear shadow-md"
+        onClick={() =>
+          handleOnClick(innerText)
+        }
+      >
+        {innerText}
+      </button>
+      <span className="text-center text-xs leading-[14px] w-10 -m-1">
+        {lowerText}
+      </span>
     </div>
   );
 };
